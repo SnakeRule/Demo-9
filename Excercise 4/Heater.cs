90cc5232614b02cbb717df354e6b7b180be69d8c
SnakeRule <jerevaltanen@gmail.com>
2016-02-10 09:32:46 +0200

    updated

---
+++ Excercise 4/Heater.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_4
+{
+    class Heater
+    {
+        private double temperature { get; set; }
+        private double humidity { get; set; }
+
+        public double Temperature
+        {
+            get
+            {
+                return temperature;
+            }
+            set
+            {
+                if (value <= 120 && value > 0)
+                    temperature = value;
+            }
+        }
+        public double Humidity { get; set; }
+
+        public double Value { get; set; }
+    }
+}
---
+++ Excercise 4/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Excercise_4
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Heater heater = new Heater();
+            bool check;
+            double value;
+            string input;
+
+            
+
+            if (temperatureRadioButton.IsChecked == true)
+            {
+                if (check = (double.TryParse(valueTextBox.Text, out value)))
+                    heater.Temperature = value;
+                else infoTextBlock.Text = "Info: Wrong value!";
+            }
+        }
+
+        private void threeButton_Click(object sender, RoutedEventArgs e)
+        {
+            string value = "3";
+            click(value);
+        }
+
+        public void click(string value)
+        {
+            if(threeButton.IsPressed == true)
+            {
+
+            }
+        }
+    }
+}
