36d618a165a5444afdc0b2ebfde2e8a186740e7d
SnakeRule <jerevaltanen@gmail.com>
2016-02-14 19:36:14 +0200

    Ready for presentation

--- Excercise 4/Heater.cs
+++ Excercise 4/Heater.cs
@@ -8,6 +8,7 @@ namespace Excercise_4
 {
     class Heater
     {
+
         private double temperature { get; set; }
         private double humidity { get; set; }
 
@@ -19,12 +20,35 @@ namespace Excercise_4
             }
             set
             {
-                if (value <= 120 && value > 0)
+                if (value <= 120)
                     temperature = value;
             }
         }
-        public double Humidity { get; set; }
+        public double Humidity
+        {
+            get
+            {
+                return humidity;
+            }
+            set
+            {
+                if (value <= 100)
+                    humidity = value;
+            }
+        }
 
         public double Value { get; set; }
+
+        public string SetTemperature(double value)
+        {
+            Temperature = value;
+            return Temperature.ToString();
+        }
+
+        public string SetHumidity(double value)
+        {
+            Humidity = value;
+            return Humidity.ToString();
+        }
     }
 }
--- Excercise 4/MainPage.xaml.cs
+++ Excercise 4/MainPage.xaml.cs
@@ -22,37 +22,93 @@ namespace Excercise_4
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        Heater heater = new Heater();
         public MainPage()
         {
             this.InitializeComponent();
+        }
+        bool check;
+        double value;
 
-            Heater heater = new Heater();
-            bool check;
-            double value;
-            string input;
 
-            
+        /// <summary>
+        /// Action when pressing the input keypad buttons
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+         private void Button_Click(object sender, RoutedEventArgs e)
+         {
+            Button button = sender as Button;
+            if (button != null)
+            {
 
+                for (int i = 0; i < 10; i++) // Checking if the input was 1-9 numbers
+                {
+                    if (button.Content.ToString() == i.ToString())
+                    {
+                        valueTextBox.Text += button.Content.ToString();
+                    }
+                }
+                if (button.Content.ToString() == "<-") // Checking if the delete input button was pressed
+                {
+                    if (valueTextBox.Text != "")
+                    {
+                        string removeChar = valueTextBox.Text.Substring(0, valueTextBox.Text.Length - 1);
+                        valueTextBox.Text = removeChar;
+                    }
+                }
+                if (button.Content.ToString() == ".") // Checking if the . button was pressed
+                    valueTextBox.Text += ".";
+            }
+        }
+
+
+        /// <summary>
+        /// Actions when pressing the ok button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void okButton_Click(object sender, RoutedEventArgs e)
+        {
             if (temperatureRadioButton.IsChecked == true)
             {
-                if (check = (double.TryParse(valueTextBox.Text, out value)))
-                    heater.Temperature = value;
-                else infoTextBlock.Text = "Info: Wrong value!";
+                if (check = (double.TryParse(valueTextBox.Text, out value))) // Checking if the temperature radio button is checked
+                {
+                    if (value <= 120)
+                    {
+                        temperatureTextBlock.Text = heater.SetTemperature(value);
+                        infoTextBlock.Text = "Info : Temperature set!";
+                    }
+                    else if (value > 120)
+                    {
+                        infoTextBlock.Text = "Info : Temperature too high -- Try a lower temperature";
+                    }
+                }
+                else
+                    infoTextBlock.Text = "Invalid input!";
             }
-        }
 
-        private void threeButton_Click(object sender, RoutedEventArgs e)
-        {
-            string value = "3";
-            click(value);
-        }
-
-        public void click(string value)
-        {
-            if(threeButton.IsPressed == true)
+            else if (humidityRadioButton.IsChecked == true) // Checking if the humidity radio button is checked
             {
-
+                if (check = (double.TryParse(valueTextBox.Text, out value)))
+                {
+                    if (value <= 100)
+                    {
+                        humidityTextBlock.Text = heater.SetHumidity(value);
+                        infoTextBlock.Text = "Info : Humidity set!";
+                    }
+                    else if (value > 100)
+                    {
+                        infoTextBlock.Text = "info : Humidity too high -- Try a lower humidity";
+                    }
+                }
+                else
+                    infoTextBlock.Text = "Invalid input!";
             }
+
+            valueTextBox.Text = ""; // Resetting the value input textbox
         }
     }
+
 }
+
